7bd8dbe7553c150d8b943403fc734764bac86b9f
Dhrubo <baridhrubo@gmail.com>
2025-02-14 02:17:18 +0600

    some more changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,14 @@
-/*===== MENU SHOW =====*/
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-      nav = document.getElementById(navId);
-    if (toggle && nav) {
-      toggle.addEventListener("click", () => {
-        nav.classList.toggle("show");
-      });
-    }
-  };
+const navToggle = document.getElementById('nav-toggle');
+const navLinks = document.querySelector('.nav-links');
 
-  window.addEventListener('scroll', function () {
-    const header = document.getElementById('header');
-    
-    // Check if the page has been scrolled down
-    if (window.scrollY > 50) { // Adjust the scroll threshold if needed
-      header.classList.add('active');
-    } else {
-      header.classList.remove('active');
-    }
-  });  
-  
-  showMenu('nav-toggle', 'nav-menu')
-  
-  /*===== REMOVE MENU MOBILE =====*/
-  const navLink = document.querySelectorAll('.nav_link');
-  const navMenu = document.getElementById('nav-menu');
-  
-  function linkAction() {
-    navLink.forEach(n => n.classList.remove('active'));
-    this.classList.add('active');
-    navMenu.classList.remove('show');
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
+
+// Optional: Close menu when clicking outside
+document.addEventListener('click', (e) => {
+  if (!navLinks.contains(e.target) && !navToggle.contains(e.target)) {
+    navLinks.classList.remove('show');
   }
-  
-  navLink.forEach(n => n.addEventListener('click', linkAction));
-  
+});
